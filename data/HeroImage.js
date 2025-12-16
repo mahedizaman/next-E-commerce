917c354d2f4a6812fcd7e91a8af26998076a9727
mahedizaman <info.zaman.profile@gmail.com>
2025-12-16 23:30:39 +0600

    add hero image & hero image data set

---
+++ data/HeroImage.js
@@ -0,0 +1,32 @@
+export  const heroImage = [
+    {
+        id:1,
+        heading:"BEAUTY GIFTS",
+        image:"/hero-image/image1.jpg"
+    },
+    {
+        id:2,
+        heading:"GIFTS FOR HER",
+        image:"/hero-image/image2.jpg"
+    },
+    {
+        id:3,
+        heading:"GIFTS FOR CHILDREN",
+        image:"/hero-image/image3.jpg"
+    },
+    {
+        id:4,
+        heading:"LUXURY GIFTS AT SEASONS.CO.UK",
+        image:"/hero-image/image4.jpg"
+    },
+    {
+        id:5,
+        heading:"GIFTS FOR HIM",
+        image:"/hero-image/image5.jpg"
+    },
+    {
+        id:6,
+        heading:"STOCKING FILLERS",
+        image:"/hero-image/image6.jpg"
+    },
+]
