cd4ea39bf4c128e5dbbe9030b3503392aaecfc63
mahedizaman <info.zaman.profile@gmail.com>
2025-12-18 00:22:24 +0600

    update nightware

--- components/Nightwear.jsx
+++ components/Nightwear.jsx
@@ -7,28 +7,31 @@ const Nightwear = () => {
         <h2 className="text-5xl md:text-6xl font-bold tracking-wide leading-none">
           nightwear
         </h2>
-
         <p className="font-semibold text-lg tracking-widest mt-4 md:mt-0">
           the perfect gift
         </p>
       </div>
 
-      <div className="relative w-full h-[50vh]">
+      <div className="relative w-full h-[30vh] md:h-[50vh]">
         <Image
           src="/image/nightwear.jpg"
           alt="nightwear"
           fill
-          className="object-cover"
+          className="object-contain"
           priority
+          quality={90}
+          sizes="100vw"
         />
       </div>
 
-      <div className="relative w-full h-[50vh]">
+      <div className="relative w-full lg:my-10 h-[30vh] md:h-[50vh]">
         <Image
           src="/image/bed-sets.jpg"
           alt="bed-sets"
           fill
-          className="object-cover"
+          className="object-contain"
+          quality={90}
+          sizes="100vw"
         />
       </div>
     </div>
