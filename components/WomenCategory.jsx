30c95081948f0dd6f83240f1a360b52bb634bb3e
mahedizaman <info.zaman.profile@gmail.com>
2025-12-19 16:44:08 +0600

    women category half  sectionadded

--- components/WomenCategory.jsx
+++ components/WomenCategory.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+"use client";
+import { womenCategory } from "@/data/WomenCategory";
+import Image from "next/image";
+import React from "react";
 
 const WomenCategory = () => {
   return (
-    <div className='py-6 px-8'>
-        <div>
-            <h3 className='uppercase font-semibold text-2xl'>shop by category</h3>
-            <div></div>
-        </div>
-    </div>
-  )
-}
+    <div className="py-6 px-8">
+      <h3 className="uppercase font-semibold text-2xl mb-6">
+        shop by category
+      </h3>
 
-export default WomenCategory
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+        {womenCategory.map((item) => (
+          <div key={item.id} className="group cursor-pointer text-center">
+            <div className="relative w-full h-[220px] overflow-hidden">
+              <Image
+                src={item.image}
+                alt={item.heading || "women category"}
+                fill
+                className="object-cover group-hover:scale-105 transition duration-300"
+              />
+            </div>
+
+            <p className="mt-3 text-sm font-medium uppercase">{item.heading}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default WomenCategory;
--- data/WomenCategory.js
+++ data/WomenCategory.js
@@ -2,56 +2,56 @@ export const womenCategory = [
   {
     id: 1,
     heading: "coats & jackets",
-    image: "/womens/image30.jpg",
+    image: "/Womens/image30.jpg",
   },
   {
     id: 2,
     heading: "tops",
-    image: "/womens/image9.jpg",
+    image: "/Womens/image9.jpg",
   },
   {
     id: 3,
     heading: "trousers",
-    image: "/womens/image28.jpg",
+    image: "/Womens/image28.jpg",
   },
   {
     id: 4,
     heading: "jeans",
-    image: "/womens/image8.jpg",
+    image: "/Womens/image8.jpg",
   },
   {
     id: 5,
     heading: "dresses",
-    image: "/womens/image11.jpg",
+    image: "/Womens/image11.jpg",
   },
   {
     id: 6,
     heading: "jumpers & cardigans",
-    image: "/womens/image6.jpg",
+    image: "/Womens/image6.jpg",
   },
   {
     id: 7,
     heading: "skirts",
-    image: "/womens/image31.jpg",
+    image: "/Womens/image31.jpg",
   },
   {
     id: 8,
     heading: "shirts & blouses",
-    image: "/womens/image13.jpg",
+    image: "/Womens/image13.jpg",
   },
   {
     id: 9,
     heading: "footwear",
-    image: "/womens/image25.jpg",
+    image: "/Womens/image25.jpg",
   },
   {
     id: 10,
     heading: "lingerie",
-    image: "/womens/image14.jpg",
+    image: "/Womens/image14.jpg",
   },
   {
     id: 11,
     heading: "beauty",
-    image: "/womens/image4.jpg",
+    image: "/Womens/image4.jpg",
   },
 ];
